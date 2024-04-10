8faf804b3523fb04d66aef7a08cc9fb532ecdfa7
Mark Clemmer <clemmerm@spu.edu>
2024-04-09 20:42:47 -0700

    Initial plotly map created of King County

--- src/Urban_Institute_Data.R
+++ src/Urban_Institute_Data.R
@@ -6,6 +6,8 @@ if (!requireNamespace("sf", quietly = TRUE)) install.packages("sf")
 library(httr)
 library(jsonlite)
 library(sf)
+library(tidyverse)
+library(plotly)
 
 # Define the URL of the GeoJSON file
 url <- "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2023/07/07/data_catalog_index_spatial.geojson"
@@ -17,5 +19,29 @@ geo_data <- st_read(url)
 # For example, to view the first few rows of the data
 head(geo_data)
 
-geo_data |>
-  filter(state_name == "Washington")
+wa_data <- geo_data |>
+filter(state_name == "Washington")
+
+ggplot(wa_data) +
+  geom_sf()
+
+king_data <- geo_data |>
+  filter(state_name == "Washington" & county_name == "King County")
+
+my_map_theme <- function(){
+  theme(panel.background=element_blank(),
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title=element_blank())
+}
+
+map <- ggplot(king_data) +
+  geom_sf(aes(fill = percent_cost_burdened_renters)) +
+  scale_fill_continuous(high = "red", low = "yellow", labels = scales::percent, breaks=c(0.001,0.30, 0.60)) +
+  my_map_theme() +
+  ggtitle("King County Percent Cost Burden")
+
+ggplotly(map) |>
+  style(hoveron = "fills")
+  
+
