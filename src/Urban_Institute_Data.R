83fc2448a8fe5663f5cdbbd1fa33fccba8d1ab39
Mark Clemmer <94164990+mclemmer7@users.noreply.github.com>
2024-04-30 22:46:39 -0700

    Script for creating ERAP minority maps

--- src/Urban_Institute_Data.R
+++ src/Urban_Institute_Data.R
@@ -8,24 +8,12 @@ library(jsonlite)
 library(sf)
 library(tidyverse)
 library(plotly)
+library(tidycensus)
 
-# Define the URL of the GeoJSON file
-url <- "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2023/07/07/data_catalog_index_spatial.geojson"
+erap_index <- st_read("erap_index.geojson")
+erap_index$census_tract <- as.numeric(substr(erap_index$GEOID, 6, 11))
 
-# Directly read the GeoJSON file into an sf object
-geo_data <- st_read(url)
-
-# Now, `geo_data` is an sf object ready for manipulation and plotting
-# For example, to view the first few rows of the data
-head(geo_data)
-
-wa_data <- geo_data |>
-filter(state_name == "Washington")
-
-ggplot(wa_data) +
-  geom_sf()
-
-king_data <- geo_data |>
+king_data <- erap_index |>
   filter(state_name == "Washington" & county_name == "King County")
 
 my_map_theme <- function(){
@@ -41,7 +29,42 @@ map <- ggplot(king_data) +
   my_map_theme() +
   ggtitle("King County Percent Cost Burden")
 
+# This is a map of King County, which gives a closer look at the data.
 ggplotly(map) |>
   style(hoveron = "fills")
-  
 
+# 53 is washington. 033 is King County and 000100 to 012100 is Seattle
+# Filter data for Seattle
+# seattle_data <- king_data |>
+#  filter(GEOID < 53033012100)
+
+seattle_data <- king_data |>
+  filter(census_tract > 000100 & census_tract < 012100)
+
+# Try writing seattle data out to a geojson file
+writeLines(seattle_data, "my_data.geojson")
+sf::st_write(seattle_data, dsn = "my_data.geojson")
+
+new_data <- st_read("my_data.geojson")
+
+# Calculate percent minority based on asians, blacks, and latinos. Not including Native Americans, Pacific Islanders and other at the moment.
+seattle_data$percent_minority <- seattle_data$percent_asian + seattle_data$percent_black + seattle_data$percent_latine
+  
+ggplot(seattle_data) +
+  geom_sf(aes(fill = percent_minority)) +
+  scale_fill_continuous(high = "red", low = "yellow", labels = scales::percent) +
+  my_map_theme() +
+  ggtitle("Seattle Minorities", subtitle = "Map of where Asian, Black, and Hispanic minorities live in Seattle")
+
+
+# Now filter data for Cleveland
+cleveland_data <- erap_index |>
+  filter(state_name == "Ohio" & county_name == "Cuyahoga County") |>
+  filter(census_tract < 150000)
+
+map <- ggplot(cleveland_data) +
+  geom_sf(aes(fill = percent_cost_burdened_renters)) +
+  scale_fill_continuous(high = "red", low = "yellow", labels = scales::percent, breaks=c(0.001,0.30, 0.60)) +
+  my_map_theme() +
+  ggtitle("King County Percent Cost Burden")
+map
